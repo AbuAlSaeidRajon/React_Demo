7172d1d103cb34d91c083dd41234dd42fba063e4
Saeid Md <khanrajon5@gmail.com>
2024-03-25 14:49:42 +0200

    button added

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,30 @@ import Box from './Box'
 import { useState } from 'react'
 
 function App() {
-  const [counter, setCounter] = useState("for example")
+  const [counter, setCounter] = useState(0)
   const [persons, setPersons] = useState([
     { id:1, name:"Saeid", title:"Full Stack Web Developer", location:"Helsinki"} , 
     { id:2, name:"Jake", title:"Developer", location:"Tampere" }, 
     { id:3, name:"Joe", title:"Designer", location:"Lappeenranta" },
   ]);
 
+  const clickHandler = () => {
+    setCounter(counter + 1)
+    };
+    const clickDeHandler = () => {
+      setCounter(counter - 1)
+      };  
+  
+      const clickZeroHandler = () => {
+        setCounter(0)
+        };     
+
   return (
     <>
     <p>Counter: {counter}</p>
+    <button onClick={clickHandler}>Increased by 1</button> <br /> <br />
+    <button onClick={clickDeHandler}>Decresed by 1</button> <br /> <br />
+    <button onClick={clickZeroHandler}>Back to 0</button>
     {persons.map((person) => (
       <Box
       key={person.id}
