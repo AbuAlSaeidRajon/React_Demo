c4fb0aaf857d5a959f8e7eeb0f88c45b20594f53
Saeid Md <khanrajon5@gmail.com>
2024-03-25 13:17:20 +0200

    useState added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 import Box from './Box'
+import { useState } from 'react'
 
 function App() {
+  const [counter, setCounter] = useState("for example")
+
   return (
     <>
-
+    <p>Counter: {counter}</p>
     <Box name="Saeid" title="Full Stack Web Developer" location="Helsinki"/>
     <Box name="Jake" title="Developer" location="Tampere"/>
     <Box name="Joe" title="Designer" location="Lappeenranta"/>
