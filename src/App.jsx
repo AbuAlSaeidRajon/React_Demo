b64596f26c01053d3098251e4971138b214fa844
Saeid Md <khanrajon5@gmail.com>
2024-03-25 12:36:39 +0200

    props added

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,10 @@ import Box from './Box'
 function App() {
   return (
     <>
-      <h1>Hello! My name is Saeid.</h1>
 
-    <Box/>
-    <Box/>
-    <Box/>
+    <Box name="Saeid" title="Full Stack Web Developer" location="Helsinki"/>
+    <Box name="Jake" title="Developer" location="Tampere"/>
+    <Box name="Joe" title="Designer" location="Lappeenranta"/>
     </>
   )
 }
